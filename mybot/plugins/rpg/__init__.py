4cf0c213c7fee547b8d86c119ec7628356ae293a
GU TIANYU <fenghuangyuanmurongqi@gmail.com>
2025-08-23 17:47:46 +0800

    功能拆分debug

--- mybot/plugins/rpg/__init__.py
+++ mybot/plugins/rpg/__init__.py
@@ -1,1 +1,15 @@
-from .rpg import *  # 把 rpg.py 里的匹配器导出
+# mybot/plugins/rpg/__init__.py
+# -*- coding: utf-8 -*-
+"""
+RPG 插件（包入口）：仅导入各功能模块以注册 matcher
+"""
+from .handlers import help  # noqa: F401
+from .handlers import profile  # noqa: F401
+from .handlers import rename  # noqa: F401
+from .handlers import list_players  # noqa: F401
+from .handlers import daily  # noqa: F401
+from .handlers import gacha  # noqa: F401
+from .handlers import refine  # noqa: F401
+from .handlers import wild  # noqa: F401
+from .handlers import pvp  # noqa: F401
+from .handlers import boss  # noqa: F401
