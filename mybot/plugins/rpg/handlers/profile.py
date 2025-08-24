4883eb285119aaef35ad129f19b9344d79c60af2
Luminaqua <elentscarlet@gmail.com>
2025-08-25 01:13:24 +0800

    #面板查询 on_keyword->on_fullmatch

--- mybot/plugins/rpg/handlers/profile.py
+++ mybot/plugins/rpg/handlers/profile.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from nonebot import on_keyword
 from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.plugin.on import on_fullmatch
+
 from ..models import get_player
 from ..utils import ids_of
 
-profile_m = on_keyword({"面板", "状态", "信息"})
-
+profile_m = on_fullmatch(("面板", "状态", "信息"))
 
 @profile_m.handle()
 async def _(event: MessageEvent):
